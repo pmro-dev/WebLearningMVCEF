fac415c81bd7638bd0c639a606f076610c2f3a8b
Piotr Markowski <markowskipriv@gmail.com>
2022-07-10 18:31:06 +0200

    ADD new MODEL class - Tournament with properties and DbSet in DbContext
    Properties:
    Id, Name, ShortDescription, LongDescription, Type, Rules, Date, EventId, Event as ForeignKey

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -13,5 +13,6 @@ namespace WebLearningMVCEF.Data
         public DbSet<User> Users { get; set; }
         public DbSet<Event> Events { get; set; }
         public DbSet<Team> Teams { get; set; }
+        public DbSet<Tournament> Tournaments { get; set; }
     }
 }
---
+++ Models/Tournament.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.IO;
+
+namespace WebLearningMVCEF.Models
+{
+    public class Tournament
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ShortDescription { get; set; }
+        public string LongDescription{ get; set; }
+        public int Type { get; set; }
+        public File Rules { get; set; }
+        public DateTime Date{ get; set; }
+
+        public int EventId { get; set; }
+        [ForeignKey("EventId")]
+        public virtual Event Event { get; set; }
+
+
+    }
+}
