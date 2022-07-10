a285f6e21792fd79cc39ff2b0d0871b5ff112f49
Piotr Markowski <markowskipriv@gmail.com>
2022-07-10 20:13:41 +0200

    EXPAND / CHANGE - User class.
    Expand / Change a properties 'RoleType' and 'CompanyId'as right implementation of ForeignKeys

--- Models/User.cs
+++ Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebLearningMVCEF.Models
 {
@@ -10,13 +11,17 @@ namespace WebLearningMVCEF.Models
         public string Email { get; set; }
         public string NickName { get; set; }
         public string Login { get; set; }
-        public int RoleType_ID { get; set; }
+        public int RoleTypeId { get; set; }
+        [ForeignKey("RoleTypeId")]
+        public virtual RoleType RoleType { get; set; }
         public string Password { get; set; }
         public string FirstName { get; set; }
         public string SecondName { get; set; }
         public int Phone { get; set; }
         public int CreditCard { get; set; }
-        public int Company_ID { get; set; }
+        public int CompanyId { get; set; }
+        [ForeignKey("CompanyId")]
+        public virtual Company Company { get; set; }
         public DateTime DateOfLastLogin { get; set; }
         public DateTime DateOfAccountCreation { get; set; }
         public DateTime DateOfGrantedRole { get; set; }
