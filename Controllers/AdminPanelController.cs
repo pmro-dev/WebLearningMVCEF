3f85e8cdc32980840bc8b9dcfe9f3310ad4bdc90
Piotr Markowski <markowskipriv@gmail.com>
2022-07-12 14:45:33 +0200

    ADD new CONTROLLER - AdminPanel includiing Index and AddUser action,
    new Index View - implementation include basic table view for User Model data and button redirecting to Add User view.
    
    new AddUser view automated generated with all actuual labels using by DataBase:
    Email, NickName, Login, RoleTypeId, Password, FirstName, SecondName, Phone, CreditCard, DateOfLastLogin, DateOfAccountCreation, DateOfGrantedRole

---
+++ Controllers/AdminPanelController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using WebLearningMVCEF.Data;
+using WebLearningMVCEF.Models;
+
+namespace WebLearningMVCEF.Controllers
+{
+    public class AdminPanelController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public AdminPanelController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<User> usersList = _db.Users;
+
+            return View(usersList);
+        }
+
+        public IActionResult AddUser()
+        {
+            return View();
+        }
+    }
+}
