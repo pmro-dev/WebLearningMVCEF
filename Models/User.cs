55b2a9573e530f5b590f8e85b60c991263f8e0be
Piotr Markowski <markowskipriv@gmail.com>
2022-07-13 09:36:29 +0200

    EXPAND class MODEL - User.
    Add Data Attributes (DisplayName and Required) to properties.
    
    Required:
    Id, Email, NickName, Login, RoleType, Password.

--- Models/User.cs
+++ Models/User.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -6,21 +7,43 @@ namespace WebLearningMVCEF.Models
 {
     public class User
     {
+        [Required]
         [Key]
         public int Id { get; set; }
+        
+        [Required]
         public string Email { get; set; }
+        
+        [Required]
+        [DisplayName("Nick")]
         public string NickName { get; set; }
+        
+        [Required]
         public string Login { get; set; }
+        
         public int RoleTypeId { get; set; }
+        
+        [Required]
         [ForeignKey("RoleTypeId")]
+        [DisplayName("Role")]
         public virtual RoleType RoleType { get; set; }
+
+        [Required]
         public string Password { get; set; }
+
+        [DisplayName("First Name")]
         public string FirstName { get; set; }
+
+        [DisplayName("Second Name")]
         public string SecondName { get; set; }
         public int Phone { get; set; }
+        [DisplayName("Credit Card")]
         public int CreditCard { get; set; }
+        [DisplayName("Las Login Date")]
         public DateTime DateOfLastLogin { get; set; }
+        [DisplayName("Account Creation Date")]
         public DateTime DateOfAccountCreation { get; set; }
+        [DisplayName("Granted Role Date")]
         public DateTime DateOfGrantedRole { get; set; }
     }
 }
